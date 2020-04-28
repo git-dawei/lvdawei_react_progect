7761a4c0ee665aab7c343880c661294e9c3eb8f7
大伟 <641394916@qq.com>
2020-04-28 12:43:36 +0800

    一级路由的创建

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import {Button} from 'antd'
+import Login from './pages/Login/Login'
+import Admin from './pages/Admin/Admin'
+import {Switch,Route, Router} from 'react-router-dom'
 
 export default class App extends Component{
   render(){
     return (
-      <div>
-        app...
-        <Button type='primary'>点我</Button>
-      </div>
+      <Switch>
+        <Route path = '/login' component = {Login}/>
+        <Route path = '/admin' component = {Admin}/>
+      </Switch>
     )
   }
 }
